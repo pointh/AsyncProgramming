f12c26315a88db76f4fe4fca11eb9c4d47b09ef7
Pavel Svec <pavel.svec@itcrawl.cz>
2019-06-14 17:19:38 +0200

    Thread reporting

--- AsyncProgramming/Program.cs
+++ AsyncProgramming/Program.cs
@@ -8,13 +8,14 @@ namespace AsyncProgramming1
         public static void Main(string[] args)
         {
             Task t = RunCountingAsync();
-
+            int i = 0;
             Console.WriteLine("Hello World!");
-            do
-            {
-                Console.WriteLine("I continue my thread.");
-            } while (t.Wait(20000));
+            do { 
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine("\nI continue main thread {0}.", System.Threading.Thread.CurrentThread.ManagedThreadId);
+            } while (++i < 20);
 
+            t.Wait(20000);
             Console.ReadLine();
         }
 
@@ -24,8 +25,8 @@ namespace AsyncProgramming1
             {
                 for (int i = 0; i < 1000; i++)
                 {
-                    Console.Write(i + " ");
-                    System.Threading.Thread.Sleep(500);
+                    Console.Write("\n{0}, side thread {1}", i, System.Threading.Thread.CurrentThread.ManagedThreadId);
+                    System.Threading.Thread.Sleep(200);
                 }
             });
         }
