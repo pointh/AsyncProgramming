f20d65d48f30b1cc640099e8a8fbf4638eeb3472
Pavel Svec <pavel.svec@itcrawl.cz>
2019-06-14 17:00:11 +0200

    Init

--- AsyncProgramming/Program.cs
+++ AsyncProgramming/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace AsyncProgramming
+namespace AsyncProgramming1
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Task t = RunCountingAsync();
+
+            Console.WriteLine("Hello World!");
+            do
+            {
+                Console.WriteLine("I continue my thread.");
+            } while (t.Wait(20000));
+
+            Console.ReadLine();
+        }
+
+        static async Task RunCountingAsync()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.Write(i + " ");
+                    System.Threading.Thread.Sleep(500);
+                }
+            });
         }
     }
 }
