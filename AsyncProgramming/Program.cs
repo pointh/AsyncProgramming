11783aee7cc824171344a9efbc73ed75fcb081d4
Pavel Svec <pavel.svec@itcrawl.cz>
2019-06-26 18:40:58 +0200

    Wait for Task<int> array

--- AsyncProgramming/Program.cs
+++ AsyncProgramming/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace AsyncProgramming1
@@ -7,28 +8,44 @@ namespace AsyncProgramming1
     {
         public static void Main(string[] args)
         {
-            Task t = RunCountingAsync();
+            Task<int> t = RunCountingAsync(0, 50);
+            Task<int> q = RunCountingAsync(51, 100);
+
+            Task[] lst = new Task[] { t, q };
+
             int i = 0;
-            Console.WriteLine("Hello World!");
             do { 
-                System.Threading.Thread.Sleep(1000);
+                System.Threading.Thread.Sleep(500);
                 Console.WriteLine("\nI continue main thread {0}.", System.Threading.Thread.CurrentThread.ManagedThreadId);
             } while (++i < 20);
 
-            t.Wait(20000);
+            Task.WaitAll(lst);
+
+            int sum = 0;
+            foreach (Task<int> tsk in lst)
+                sum += tsk.Result;
+
+            Console.WriteLine($"\nSoučet je {sum}");
+            
             Console.ReadLine();
         }
 
-        static async Task RunCountingAsync()
+        static async Task<int> RunCountingAsync(int k, int l)
         {
+            int sum = 0;
+
             await Task.Run(() =>
             {
-                for (int i = 0; i < 1000; i++)
+                for (int i = k; i <= l; i++)
                 {
-                    Console.Write("\n{0}, side thread {1}", i, System.Threading.Thread.CurrentThread.ManagedThreadId);
+                    Console.Write("\n{0}, side thread {1}, pracuji na součtu od {2} do {3}", 
+                        i, System.Threading.Thread.CurrentThread.ManagedThreadId, k, l);
                     System.Threading.Thread.Sleep(200);
+                    sum += i;
                 }
+                
             });
+            return sum;
         }
     }
 }
